5d38870be84b1470418b9291c849a249eb3545f0
dpfaffenbauer <dominik@lineofcode.at>
2017-01-10 10:52:22 +0100

    implement Datatrans Lightbox Gateway

---
+++ lib/Omnipay/Datatrans/LightboxGateway.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Omnipay\Datatrans;
+
+/**
+ * Datatrans Gateway
+ */
+class LightboxGateway extends Gateway
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Datatrans Lightbox';
+    }
+}
